05976e3b444b58837daf5d84a27b5ad8fc1ee89a
Anton Yatskevich <Anton_Yatskevich@epam.com>
2018-09-05 13:02:26 +0300

    feat: define and store device type

--- src/js/components/App/app.controller.js
+++ src/js/components/App/app.controller.js
@@ -1,13 +1,17 @@
+import angular from 'angular';
 import API_URL from '../../constants/API_URL';
 import * as CardFilterActions from '../../actions/CardFiltersActions';
+import * as DeviceTypeAction from '../../actions/DeviceTypeActions';
 import CARD_TYPES from '../../constants/CARD_TYPES';
 import SORT_TYPES from '../../constants/SORT_TYPES';
+
 
 const mapStateToThis = (state) => {
 	const { cards, isLoading } = state.cards;
 	const {
 		cardType, costAmount, costInMonth, sortType,
 	} = state.cardFilters;
+	const { minWidth, maxWidth } = state.device;
 	return {
 		cards,
 		costAmount,
@@ -15,21 +19,36 @@ const mapStateToThis = (state) => {
 		sortType,
 		cardType,
 		isLoading,
+		minWidth,
+		maxWidth,
 	};
 };
 
 class AppController {
-	constructor($ngRedux, $scope, FetchCards) {
+	constructor($ngRedux, $scope, $window, FetchCards) {
+		this.window = $window;
 		this.$ngRedux = $ngRedux;
 		this.FetchService = FetchCards;
 		this.cardTypes = CARD_TYPES;
 		this.sortTypes = SORT_TYPES;
-		const unsubscribe = this.$ngRedux.connect(mapStateToThis, CardFilterActions)(this);
+		const unsubscribe = this.$ngRedux.connect(
+			mapStateToThis,
+			Object.assign({}, DeviceTypeAction, CardFilterActions),
+		)(this);
 		$scope.$on('$destroy', unsubscribe);
 	}
 
 	$onInit() {
 		this.$ngRedux.dispatch(this.FetchService.fetchCards(API_URL));
+		this.setDeviceType(this.window.innerWidth);
+		angular.element(this.window).on('resize', () => this.onResize());
+	}
+
+	onResize() {
+		const width = this.window.innerWidth;
+		if (width < this.minWidth || width > this.maxWidth) {
+			this.setDeviceType(width);
+		}
 	}
 
 	setCardType(type) {
---
+++ src/js/constants/DEVICES_WIDTH.js
@@ -0,0 +1,19 @@
+const DEVICES_WIDTH = [
+	{
+		deviceName: 'mobile',
+		minWidth: 320,
+		maxWidth: 768,
+	},
+	{
+		deviceName: 'tablet',
+		minWidth: 768,
+		maxWidth: 1200,
+	},
+	{
+		deviceName: 'desktop',
+		minWidth: 1200,
+		maxWidth: +Infinity,
+	},
+];
+
+export default DEVICES_WIDTH;
